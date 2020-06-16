9d7d11a1c1450cb67d06ab77606c724b17e66721
chezziechester <wanning.lu@gmail.com>
2020-06-15 17:20:11 -0700

    it doesn't work

---
+++ content.js
@@ -0,0 +1,8 @@
+
+let limit = prompt("How many articles are you reading today?");
+//asks for and records personalized reading goal
+let numArticles = 0; //number of articles opened/read
+//every time the user opens an article
+//should we only count an article if the user spends more than, say, 1 minute reading it?
+numArticles++;
+//this is just a very vague idea for logging the number of articles
