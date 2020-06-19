096bca320a8ccde9e247c699de24e2a443577e65
ax-md <62274777+ax-md@users.noreply.github.com>
2020-06-18 19:42:18 -0700

    moved article functionality to popup.js

--- content.js
+++ content.js
@@ -1,12 +1,5 @@
 
 //ARTICLE FUNCTIONALITY
-let limit = prompt("How many articles are you reading today?");
-//asks for and records personalized reading goal
-var numArticles = 0; //number of articles opened/read
-//every time the user opens an article
-//should we only count an article if the user spends more than, say, 1 minute reading it?
-numArticles++;
-//this is just a very vague idea for logging the number of articles
 
 //TIMER FUNCTIONALITY
 var start = Date.now();
