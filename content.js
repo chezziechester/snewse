955ce0c26594cc2d4bd4cdc9595d39870c362662
chezziechester <wanning.lu@gmail.com>
2020-06-19 17:15:26 -0700

    used chrome's storage api to store the time value

--- content.js
+++ content.js
@@ -1,26 +1,39 @@
 
-//ARTICLE FUNCTIONALITY
-
 //TIMER FUNCTIONALITY
-var start = Date.now();
-var seconds = 0
-var minutes = 0
-var hours = 0
+
+//"starting time" for when the user first visited a news website
+var start
+//used as reference to calculate the other values
+var tmpSeconds
+var seconds
+var minutes
+var hours
+
+var defaultValue = Date.now()
+chrome.storage.sync.get({startTime: defaultValue}, function(data) {
+  //if data.startTime is set, then it will use that value
+  //otherwise, it will use the default value
+  chrome.storage.sync.set({startTime: data.startTime}, function() {
+    start = data.startTime
+  });
+});
 
 //updates every second
 setInterval(function() {
   //gets the difference in milliseconds from the starting time
   //then converts to seconds
-  seconds = Math.round((Date.now() - start) / 1000)
-  //making sure to update if either seconds or minutes hit 60
-  if (seconds >= 60) {
-    minutes++
-    seconds = 0
-    start = Date.now()
+  //only temporary because i need it for the other values
+  tmpSeconds = Math.round((Date.now() - start) / 1000)
+  //the actual seconds value
+  seconds = tmpSeconds % 60
+  //first check the value isn't under 1
+  if (((tmpSeconds / 60) % 60) >= 1) {
+    //get number of minutes first
+    //then get the remainder when divided by 60 (as to not count the hours)
+    minutes = Math.floor((tmpSeconds / 60) % 60)
   }
-  if (minutes >= 60) {
-    hours++
-    minutes = 0
+  if (((tmpSeconds / 60) / 60) >= 1 ) {
+    hours = Math.floor((tmpSeconds / 60) / 60)
   }
 
   document.getElementById("timerText").innerHTML = `${hours}h ${minutes}m ${seconds}s`
