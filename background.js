18b57e6ebd792037cc048a471d8058b79084c763
ax-md <62274777+ax-md@users.noreply.github.com>
2020-06-21 02:02:47 -0700

    updated bg.js

--- background.js
+++ background.js
@@ -13,9 +13,28 @@ chrome.runtime.onInstalled.addListener(function() {
       })],
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }
-  ]);
-  });
-});
+  ])
+  })
+  chrome.storage.sync.set({limitReached: false})
+  chrome.storage.sync.set({limit: 0})
+})
+
+function limitReached() {
+  console.log("hi, at least you got here!")
+  var limit
+  chrome.storage.sync.get(['limit'], function(data) {
+    limit = data.limit
+  })
+  chrome.storage.sync.get(['articles'], function(data) {
+    if (data.length >= limit) {
+      console.log(data.length)
+      chrome.storage.sync.set({limitReached: true})
+      console.log("THIS WORKS")
+    } else {
+      alert("You need to set a limit!")
+    }
+  })
+}
 
 //communicate between background and content script because apparently
 //you can't use the tabs api nor the storage api in the content script
@@ -24,7 +43,26 @@ chrome.runtime.onInstalled.addListener(function() {
 chrome.runtime.onMessage.addListener(
   function(message, sender, sendResponse) {
     if (message === "article visited") {
-      sendResponse(sender.url)
-      return true
+      var url = sender.url
+      var articleArray
+      var repeated
+
+      chrome.storage.sync.get({articles: []}, function(data) {
+        chrome.storage.sync.set({articles: data.articles}, function() {
+          articleArray = data.articles
+          //checks to make sure an article isn't repeated
+          for (indexNumber in articleArray) {
+            if (url === articleArray[indexNumber]) {
+              repeated = true
+            }
+          }
+          console.log(articleArray)
+          if (!repeated) {
+            articleArray.push(url)
+            chrome.storage.sync.set({articles: articleArray})
+            limitReached()
+          }
+        })
+      })
     }
 })
